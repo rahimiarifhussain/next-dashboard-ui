715245c92b0ac0619ded1800fd63b21514d0b318
Arif Hussain Rahimi <arifhussain.rahimi84@gmail.com>
2025-02-25 15:11:12 +0430

    working on Teacher, Parent and Student pages

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,9 +1,34 @@
+import Announcements from "@/components/Announcements";
+import BigCalender from "@/components/BigCalender";
+import EvenCalendar from "@/components/EvenCalendar";
 import React from 'react'
+import { calendarEvents } from "@/lib/data";
 
-const Page = () => {
+const StudentPage = () => {
   return (
-    <div>Student Pages</div>
-  )
+    <div className="flex gap-4 flex-col xl:flex-row">
+      {/* Left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1
+            className="text-sl font-semibold
+           "
+          >
+            Schedule(4A){" "}
+          </h1>
+          <BigCalender />
+        </div>
+      </div>
+
+      {/* Right */}
+      <div className=" w-full xl:w-1/3 flex  flex-col gap-8">
+        <div className="">
+          <EvenCalendar />
+          <Announcements />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Page
+export default StudentPage;
