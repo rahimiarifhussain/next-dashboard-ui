ca5c8a8767cdb29eb3e669ab05d95ed14bede2e6
Arif Hussain Rahimi <arifhussain.rahimi84@gmail.com>
2025-04-02 12:20:11 +0430

    work  on the sujbect

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -8,7 +8,7 @@ import { ITEM_PER_PAGE } from "@/lib/settings";
 import { Class, Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+
 // import { clearScreenDown } from "readline";
 
 type TeacherList = Teacher & { subjects: Subject[] } & { classes: Class[] };
