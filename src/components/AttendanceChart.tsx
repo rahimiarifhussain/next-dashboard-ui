c69b1969cf785e3bedd817a4b6836026e0823bf4
Arif Hussain Rahimi <arifhussain.rahimi84@gmail.com>
2025-04-06 08:32:10 +0430

    install clerk

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -48,7 +48,7 @@ const AttendanceChart = () => {
         <Image src="/moreDark.png" alt="" width={20} height={20} />
       </div>
       <ResponsiveContainer width="100%" height="90%">
-        <BarChart width={500} height={300} data={data} barSize={20}>
+        <BarChart  height={300} data={data} barSize={20}>
           <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
           <XAxis
             dataKey="name"
